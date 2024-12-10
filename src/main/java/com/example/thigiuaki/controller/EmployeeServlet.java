bf1ca92e53cdfe643430a5682a6054f21568fc28
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2024-12-10 16:06:58 +0700

    do get and do post

---
+++ src/main/java/com/example/thigiuaki/controller/EmployeeServlet.java
@@ -0,0 +1,78 @@
+package com.example.thigiuaki.controller;
+
+import com.example.thigiuaki.model.Employee;
+import com.example.thigiuaki.service.EmployeeService;
+import com.example.thigiuaki.service.EmployeeServiceImpl;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "EmployeeServlet", urlPatterns = "/employees")
+public class EmployeeServlet extends HttpServlet {
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                createCustomer(request, response);
+                break;
+            case "edit":
+                updateCustomer(request, response);
+                break;
+            case "delete":
+                deleteCustomer(request, response);
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "create":
+                showCreateForm(request, response);
+                break;
+            case "edit":
+                showEditForm(request, response);
+                break;
+            case "delete":
+                showDeleteForm(request, response);
+                break;
+            case "view":
+                viewCustomer(request, response);
+                break;
+            default:
+                listCustomers(request, response);
+                break;
+        }
+    }
+
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Employee> employees = this.employeeService.employeeList();
+        request.setAttribute("employees", employees);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("EmployeeList.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
